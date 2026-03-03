d19fd9b0c8c01bd0e8f47e66b7a4c11c869e8dfe
Nerd452 <emmettkop6@gmail.com>
2026-03-03 15:30:00 -0800

    added more functionality to the output class

--- src/graphics/output.java
+++ src/graphics/output.java
@@ -5,16 +5,33 @@ package graphics;
 
 
 /**
- *This interface gives
+ *This interface gives several powerful tools for quickly and easily writing to the console
  */
 public class output{
 
     /**
-     *writes a line to the console
+     *writes to the console
+     * @param arg ~ the string written to the console
      */
     public static void write(String arg){
         System.out.print(arg);
     };
+
+    /**
+     *writes a newline to the console
+     */
+    public static void newLine(){
+        write("\n");
+    }
+
+    /**
+     * writes a string to the console, followed by a newline
+     * @param arg ~ the string written to the console
+     */
+    public static void writeln(String arg){
+        write(arg);
+        newLine();
+    }
 
 
 }
